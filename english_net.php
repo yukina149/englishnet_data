0b8eb4034c41d377f3983e759391ba7202fe395c
yukina <janet930125@gmail.com>
2024-12-19 00:43:46 +0800

    搞定按鈕問題

--- english_net.php
+++ english_net.php
@@ -10,8 +10,12 @@
     <section id="test">
         <h2>測驗</h2>
     <?php
+    //設定分數跟題數
+        $score=0;
+        $correct_sum=0;
         session_start();
-
+        
+        
             //資料庫的連接
             $servername = "my-db1.c4li6gbfgebb.us-east-1.rds.amazonaws.com";
             $username = "admin";
@@ -36,12 +40,17 @@
         // Fetch questions
         $sql = "SELECT * FROM quiz_questions LIMIT $current_index, 1";
         $result = $conn->query($sql);
+        
+
 
         if ($result->num_rows > 0) {
             $question = $result->fetch_assoc();
         } else {
             echo "<p>測驗結束！</p>";
-            echo '<button onclick="location.href=\'index.php\'">回到首頁</button>';
+            echo "分數是 $score <br>";
+            echo "答對了： $correct_sum 題<br>";
+            echo '<button onclick="location.href=\'index.php\'" class="btn btn-dark">回到首頁</button>';
+            session_unset();
             session_destroy();
             exit;
         }
@@ -49,23 +58,28 @@
         $user_answer = $_POST['answer'] ?? null;
         $feedback = "";
         $show_next_button = false;
+        $show_submit_button = true;
+
 
         if ($user_answer) {
             $correct_answer = $question['correct_answer'];
             if ($user_answer === $correct_answer) {
+                $score+=10;
+                $correct_sum+=1;
                 $feedback = "<p>恭喜答對！</p>";
             } else {
-                $feedback = "<p>這題答案是: $correct_answer</p>";
+                $feedback = "<p>不正確，這題答案是: $correct_answer</p>";
             }
             //顯示下一題的按鈕
             $show_next_button = true;
+            $show_submit_button = false;
         }
         if (isset($_POST['next'])) {
             $_SESSION['current_question_index']++;
             header("Location: " . $_SERVER['PHP_SELF']);
             exit;
         }
-        // Handle restart action
+        // 原本是要寫按下按鈕從頭開始就會清空session但是按鈕的位置就不能下移，尚未解決 哭
         /*if (isset($_POST['restart'])) {
         session_destroy();
         //導回網頁
@@ -100,30 +114,32 @@
                     <input type="radio" name="answer" value="D" <?php if ($user_answer === 'D') echo 'checked'; ?>>
                     <?php echo $question['option_d']; ?>
                 </label><br><br>
-                <div class="container">
-                    <div class="row">
-                        <div class="button-group">
-                            <button type="submit" class="btn btn-light">提交答案</button>
-                        
-                            <?php if ($show_next_button) { ?>
-                                <form method="post" style="margin-top: 10px display: inline;">
-                                    <button type="submit" name="next" class="btn btn-dark">下一題</button>
-                                </form>
-                            <?php } ?>
+ 
 
-                        </div>   
-                        
-                    </div>
-                   
+                        <?php if ($show_submit_button) { ?>
+                            <form method="post" style="display: inline;">
+                                <button type="submit" class="btn btn-light">提交答案</button>
+                            </form>
+                        <?php } ?>
+                        <?php if ($show_next_button) { ?>
+                            <form method="post" style="display: inline;">
+                                <button type="submit" name="next" class="btn btn-dark">下一題</button>
+                            </form>
+                        <?php } ?>
+
+
+
+            </form>
+            <br>
+            <?php if ($feedback) echo $feedback; ?> 
+            <div class="button-group">
+                <div class="row">
+                    <form method="post" action="test_start.php" style="display: inline;">
+                        <button type="submit" class="btn btn-secondary" name="restart">從頭開始</button>
+                    </form>
                 </div>
-            </form>
-            <br>
-            <?php if ($feedback) echo $feedback; ?>
-            <br>
-            <form method="post" action="test_start.php" style="display: inline;">
-                <button type="submit" class="btn btn-secondary margin-top: 10px display: inline;" name="restart">從頭開始作答</button>
-            </form>
-            
+            </div>
+           
         </div>
     </section>
 </main>
