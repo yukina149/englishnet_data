fcfaa42cf353e3b0b276d5d9019695690637510f
yukina <janet930125@gmail.com>
2024-12-18 22:03:30 +0800

    把每個檔引入的header全部改成html

--- english_net.php
+++ english_net.php
@@ -1,4 +1,4 @@
-<?php include('header.php'); ?>
+<?php include('header.html'); ?>
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <main>
